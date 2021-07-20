e7c560c4db64653fa063715a4770a447c865ce9b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-07-20 01:29:25 +0100

    Update R/functions/isdependentNC.R

--- R/functions/isdependentNC.R
+++ R/functions/isdependentNC.R
@@ -1,4 +1,14 @@
+# Title: Dependency Analysis
+# Objective: Dependency analysis w.r.t. a numerical field & a categorical target
+# Created by: greyhypotheses
+# Created on:
+
 isdependentNC <- function(attributes, label, frame) {
+  #' Returns a table of test statistic, p, & Cramér's V values
+  #'
+  #' @param attributes: The list of categorical fields
+  #' @param label: The name of the target field
+  #' @param frame: The table of data
   
   estimates <- data.table()
   for (attribute in attributes) {
