41f6a057eb004e0fe129b3dd7b0b69ab300f1904
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-07-20 02:09:07 +0100

    Update R/main.R

--- R/main.R
+++ R/main.R
@@ -3,43 +3,60 @@
 # Created by: greyhypotheses
 # Created on:
 
-# Functions
-source("R/functions/ImportPackages.R")
-source("R/functions/IsDependentCC.R")
-source("R/functions/IsDependentNC.R")
+main <- function () {
 
-# Ensure that the required packages are available
-packages()
+  # Functions
+  source("R/functions/ImportPackages.R")
+  source("R/functions/IsDependentCC.R")
+  source("R/functions/IsDependentNC.R")
+  source("R/functions/ExportData.R")
 
-# Libraries
-library(jsonlite)
-library(httr)
-library(rcompanion)
+  # Ensure that the required packages are available
+  packages()
 
-# These will be the YAML file entities
-classes <- c("integer", "integer", "integer", "integer", "integer", "integer", "integer", "factor", "factor", "factor",
-            "factor", "factor", "factor", "factor", "factor", "factor", "factor", "factor", "factor", "factor", "factor", "factor")
-dataurl <- 'https://raw.githubusercontent.com/exhypotheses/risk/develop/warehouse/data/baseline.csv'
-propertiesurl <- 'https://raw.githubusercontent.com/exhypotheses/risk/develop/warehouse/data/fields.json'
+  # Libraries
+  library(jsonlite)
+  library(httr)
+  library(rcompanion)
 
-# Data
-baseline <- fread(dataurl, header = TRUE, encoding = "UTF-8", data.table = TRUE, 
+  # These will be the YAML file entities
+  classes <- c("integer", "integer", "integer", "integer", "integer", "integer", "integer", "factor", "factor", "factor",
+               "factor", "factor", "factor", "factor", "factor", "factor", "factor", "factor", "factor", "factor", "factor", "factor")
+  dataurl <- 'https://raw.githubusercontent.com/exhypotheses/risk/develop/warehouse/data/baseline.csv'
+  propertiesurl <- 'https://raw.githubusercontent.com/exhypotheses/risk/develop/warehouse/data/fields.json'
+
+  # Data
+  baseline <- fread(dataurl, header = TRUE, encoding = "UTF-8", data.table = TRUE,
                     colClasses = classes)
 
-# A dictionary of the data fields
-request <- httr::GET(url = propertiesurl)
-httr::http_type(request)
-fields <- jsonlite::fromJSON(txt = content(request, as = 'text', encoding = 'UTF-8'), simplifyVector = TRUE)
+  # A dictionary of the data fields
+  request <- httr::GET(url = propertiesurl)
+  httr::http_type(request)
+  fields <- jsonlite::fromJSON(txt = content(request, as = 'text', encoding = 'UTF-8'), simplifyVector = TRUE)
 
-# The fields w.r.t. the data set in focus (ref. the data URL)
-indices <- seq(fields$source)
-index <- indices[fields$source == basename(dataurl)]
-categorical_ <- fields$properties$categoricalFields[[index]]
-numeric_ <- fields$properties$numeric[[index]]
-target_ <- fields$properties$target[[index]]
+  # The fields w.r.t. the data set in focus (ref. the data URL)
+  indices <- seq(fields$source)
+  index <- indices[fields$source == basename(dataurl)]
+  categorical_ <- fields$properties$categoricalFields[[index]]
+  numeric_ <- fields$properties$numeric[[index]]
+  target_ <- fields$properties$target[[index]]
 
-# The degree of dependence between target & categorical variables
-c_categorical <- IsDependentCC(variables = categorical_, target = target_, frame = baseline)
+  # The degree of dependence between target & categorical variables
+  c_categorical <- IsDependentCC(variables = categorical_, target = target_, frame = baseline)
 
-# Next, for each target & numeric variable pairing determine the correlation values
-n_categorical <- IsDependentNC(variables = numeric_, target = target_, frame = baseline)
+  # Next, for each target & numeric variable pairing determine the correlation values
+  n_categorical <- IsDependentNC(variables = numeric_, target = target_, frame = baseline)
+
+  # Prepare directory
+  pathstr <- file.path(getwd(), 'warehouse', 'dependencies')
+  if (dir.exists(paths = pathstr)) {
+    base::unlink(pathstr, recursive = TRUE)
+  }
+  dir.create(path = pathstr, showWarnings = TRUE, recursive = TRUE)
+
+  # Save
+  mapply(ExportData, x = list(c_categorical, n_categorical), file_name = list('categorical.csv', 'numerical.csv'), pathstr =
+    pathstr)
+
+
+}
